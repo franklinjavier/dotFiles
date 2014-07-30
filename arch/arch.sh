c17c2690b5e786d42708ad4db90e42469cfd3e5e
Franklin Javier <franklinjalves@gmail.com>
2014-07-29 22:56:41 -0300

    Arch installation

--- arch/arch.sh
+++ arch/arch.sh
@@ -5,8 +5,20 @@ pacman -Syy
 yaourt -Syua
 
 # Install all apps
-pacman -S git gvim ruby dmenu nodejs i3 dmenu
-yaourt -S google-chrome chromium chromium-pepper-flash svn feh scrot lxappearance zsh openssh --noconfirm
+yaourt -S git gvim ruby dmenu nodejs i3 dmenu google-chrome chromium chromium-pepper-flash svn feh scrot lxappearance zsh openssh --noconfirm
+
+git config --global user.email "franklinjalves@gmail.com"
+git config --global user.name "Franklin Javier"
+git config --global http.sslVerify false
+
+mkdir ~/r00t
+mkdir ~/.i3
+git clone https://github.com/franklinjavier/dotFiles.git ~/r00t/dotFiles
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s ~/r00t/dotFiles/.vim ~/.vim
+ln -s ~/r00t/dotFiles/i3/config ~/.i3/config
+rm ~/.config/xfce4/terminal/terminalrc
+ln -s ~/r00t/dotFiles/terminalrc ~/.config/xfce4/terminal/terminalrc
 
 # Oh my zsh
 curl -L http://install.ohmyz.sh | sh
@@ -17,12 +29,3 @@ ln -s ~/r00t/dotFiles/oh-my-zsh/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.
 ln -s ~/r00t/dotFiles/fonts/* /usr/share/fonts/
 fc-cache -fv
 # inserir zsh no final do ~/.bashrc
-
-# Cloning all repos
-mkdir ~/r00t
-git clone https://github.com/franklinjavier/dotFiles.git ~/r00t/dotFiles
-ln -s ~/r00t/dotFiles/.vim ~/.vim
-ln -s ~/r00t/dotFiles/i3/config ~/.i3/config
-rm ~/.config/xfce4/terminal/terminalrc
-ln -s ~/r00t/dotFiles/terminalrc ~/.config/xfce4/terminal/terminalrc
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
