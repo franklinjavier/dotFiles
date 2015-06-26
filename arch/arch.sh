ca5a9344f7db0aef9cddba2f6c5f5c748200a16e
Franklin Javier <franklinjalves@gmail.com>
2015-06-26 12:33:02 -0300

    Add softwares to install

--- arch/arch.sh
+++ arch/arch.sh
@@ -5,7 +5,7 @@ pacman -Syy
 yaourt -Syua
 
 # Install all apps
-yaourt -S git gvim ruby dmenu nodejs i3 dmenu google-chrome chromium chromium-pepper-flash svn feh scrot lxappearance zsh openssh cmus --noconfirm
+yaourt -S git gvim ruby dmenu nodejs i3 dmenu google-chrome chromium chromium-pepper-flash svn feh scrot lxappearance zsh openssh cmus ack-grep silversearcher-ag  --noconfirm
 
 # Git configs
 git config --global user.email "franklinjalves@gmail.com"
@@ -28,7 +28,7 @@ ln -s ~/b0x/dotFiles/terminalrc ~/.config/xfce4/terminal/terminalrc
 curl -L http://install.ohmyz.sh | sh
 rm ~/.zshrc
 ln -s ~/b0x/dotFiles/oh-my-zsh/zshrc ~/.zshrc
-rm ~/.oh-my-zsh/themes/agnoster.zsh-theme 
+rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
 ln -s ~/b0x/dotFiles/oh-my-zsh/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 ln -s ~/b0x/dotFiles/fonts/* /usr/share/fonts/
 fc-cache -fv
