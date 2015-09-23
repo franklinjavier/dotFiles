b56caeec510fb8d372bebff739bf3d3106fbaa7e
Franklin Javier <franklinjalves@gmail.com>
2015-09-23 11:33:40 -0300

    Create db folder for mongo

--- scripts/mongo-linux.sh
+++ scripts/mongo-linux.sh
@@ -1,8 +1,13 @@
 MONGO_PATH="/home/$(whoami)/mongodb"
 
+
 echo 'Creating structure...'
 mkdir -p $MONGO_PATH
 cd $MONGO_PATH
+
+sudo mkdir -p /data/db
+sudo chmod 0755 /data/db
+sudo chown $USER /data/db
 
 echo 'Downloading...'
 curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.6.tgz
