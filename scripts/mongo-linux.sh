c71d18a96a9d0de0aa7925dc5e0d68de1b19d3b3
Franklin Javier <franklinjalves@gmail.com>
2015-09-23 11:14:27 -0300

    Script to install mongodb on ubuntu

---
+++ scripts/mongo-linux.sh
@@ -0,0 +1,20 @@
+MONGO_PATH="/home/$(whoami)/mongodb"
+
+echo 'Creating structure...'
+mkdir -p $MONGO_PATH
+cd $MONGO_PATH
+
+echo 'Downloading...'
+curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.6.tgz
+
+echo 'Unziping...'
+tar -zxvf mongodb-linux-x86_64-3.0.6.tgz
+
+echo 'Configuring...'
+mv mongodb-linux-x86_64-3.0.6/* .
+rm -rf mongodb-linux-x86_64-3.0.6
+echo 'export PATH=/home/$(whoami)/mongodb/bin:$PATH' >> ~/.zshrc
+
+echo 'Done!\n\n'
+
+mongo
